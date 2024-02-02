e82955354986a191cac616502b7016dd40373fa9
Kimmingki <rlaalsrl9569@naver.com>
2024-02-02 13:12:50 +0900

    RedirectAttributes

--- item-service/src/main/java/hello/itemService/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemService/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -53,55 +54,58 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+////    @PostMapping("/add")
+//    public String addItemV1(@RequestParam(value = "itemName") String itemName,
+//                       @RequestParam(value = "price") Integer price,
+//                       @RequestParam(value = "quantity") Integer quantity,
+//                       Model model) {
+//
+//        Item item = new Item(itemName, price, quantity);
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+
+////    @PostMapping("/add")
+//    public String addItemV2(@ModelAttribute(value = "item") Item item) {
+//        itemRepository.save(item);
+//
+////        model.addAttribute("item", item);     자동 추가, 생략 가능
+//
+//        return "basic/item";
+//    }
+
 //    @PostMapping("/add")
-    public String addItemV1(@RequestParam(value = "itemName") String itemName,
-                       @RequestParam(value = "price") Integer price,
-                       @RequestParam(value = "quantity") Integer quantity,
-                       Model model) {
-
-        Item item = new Item(itemName, price, quantity);
-        itemRepository.save(item);
-
-        model.addAttribute("item", item);
-
-        return "basic/item";
-    }
+//    public String addItemV3(@ModelAttribute Item item) {
+//        itemRepository.save(item);
+//
+//        return "basic/item";
+//    }
 
 //    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute(value = "item") Item item) {
-        itemRepository.save(item);
-
-//        model.addAttribute("item", item);     자동 추가, 생략 가능
-
-        return "basic/item";
-    }
+//    public String addItemV4(Item item) {
+//        itemRepository.save(item);
+//
+//        return "basic/item";
+//    }
 
 //    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item) {
-        itemRepository.save(item);
+//    public String addItemV5(Item item) {
+//        itemRepository.save(item);
+//
+//        return "redirect:/basic/items/" + item.getId();
+//    }
 
-//        model.addAttribute("item", item);     자동 추가, 생략 가능
-
-        return "basic/item";
-    }
-
-    //@PostMapping("/add")
-    public String addItemV4(Item item) {
-        itemRepository.save(item);
-
-        return "basic/item";
-    }
-
-    /**
-     * 상품 등록 로직
-     * @param item 상품 등록 정보
-     * @return
-     */
     @PostMapping("/add")
-    public String addItemV5(Item item) {
-        itemRepository.save(item);
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
 
-        return "redirect:/basic/items/" + item.getId();
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
