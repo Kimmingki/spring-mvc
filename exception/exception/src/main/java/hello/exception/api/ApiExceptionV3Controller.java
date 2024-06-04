76183d9ea1a0039284aefddeef62c55ab4b510f4
Kimmingki <rlaalsrl9569@naver.com>
2024-06-04 16:23:06 +0900

    @ControllerAdvice

--- exception/exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
+++ exception/exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -4,11 +4,13 @@ import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class ApiExceptionV2Controller {
+public class ApiExceptionV3Controller {
 
     @Data
     @AllArgsConstructor
@@ -17,7 +19,7 @@ public class ApiExceptionV2Controller {
         private String name;
     }
 
-    @GetMapping("/api2/members/{id}")
+    @GetMapping("/api3/members/{id}")
     public MemberDTO getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
